6b8ca2f601fba84fd3e8972022cf9bc4002cf8c9
Legendary Leo <LegendaryLeoGG@gmail.com>
2026-04-01 08:08:09 -0400

    feat: add canonical PA county list (shared source of truth)

---
+++ js/counties.js
@@ -0,0 +1,19 @@
+// js/counties.js
+// Canonical list of 67 PA counties. This is the single source of truth for the frontend.
+// The Worker has an identical copy at workers/src/counties.js.
+const PA_COUNTIES = [
+  "Adams", "Allegheny", "Armstrong", "Beaver", "Bedford",
+  "Berks", "Blair", "Bradford", "Bucks", "Butler",
+  "Cambria", "Cameron", "Carbon", "Centre", "Chester",
+  "Clarion", "Clearfield", "Clinton", "Columbia", "Crawford",
+  "Cumberland", "Dauphin", "Delaware", "Elk", "Erie",
+  "Fayette", "Forest", "Franklin", "Fulton", "Greene",
+  "Huntingdon", "Indiana", "Jefferson", "Juniata", "Lackawanna",
+  "Lancaster", "Lawrence", "Lebanon", "Lehigh", "Luzerne",
+  "Lycoming", "McKean", "Mercer", "Mifflin", "Monroe",
+  "Montgomery", "Montour", "Northampton", "Northumberland", "Perry",
+  "Philadelphia", "Pike", "Potter", "Schuylkill", "Snyder",
+  "Somerset", "Sullivan", "Susquehanna", "Tioga", "Union",
+  "Venango", "Warren", "Washington", "Wayne", "Westmoreland",
+  "Wyoming", "York"
+];
